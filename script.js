3f298fe2f94c2400a61db6ab45838757d9bd75b6
Nellie <fabybacenelson@gmail.com>
2019-11-19 00:22:27 -0700

    added display q and a functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let start = document.getElementById("start");
-    let hScore = document.getElementById("hScore");
+    // let hScore = document.getElementById("hScore");
     let time = document.getElementById("time");
+    let quest = document.getElementById("question");
+    let answers = document.getElementById("answers");
 
 
     start.addEventListener("click", startQuiz);
@@ -10,6 +12,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
         start.style.display = "none";
         console.log("hello");
         setTime();
+        displayQuestion();
+        displayAnswers();
+    }
+    
+
+    function displayQuestion(){
+        quest.textContent = questions[0].title;
+    }
+
+    function displayAnswers(){
+        for(let i = 0; i < 4 ; i++){
+            console.log(i);
+            let li = document.createElement("li");
+            console.log(li);
+            li.textContent = questions[0].choices[i];
+            answers.append(li);
+        }
     }
 
     // 15 seconds per question
