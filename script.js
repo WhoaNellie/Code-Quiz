e953843a220d2e845960a34d9f6905b59c933b51
Nellie <fabybacenelson@gmail.com>
2019-11-19 00:43:46 -0700

    got answer check working

--- script.js
+++ script.js
@@ -16,26 +16,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
         displayAnswers();
     }
     
-
-    function displayQuestion(){
-        quest.textContent = questions[0].title;
-    }
-
-    function displayAnswers(){
-        for(let i = 0; i < 4 ; i++){
-            console.log(i);
-            let li = document.createElement("li");
-            console.log(li);
-            li.textContent = questions[0].choices[i];
-            answers.append(li);
-        }
-    }
-
-    // 15 seconds per question
-    let countdown = 10;
+    let countdown = 15;
 
     function setTime(){
-        let timer = setInterval( function (){
+        let timer = setInterval(function (){
           
           countdown--;
       
@@ -48,6 +32,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
           }
       
         }, 1000)
-      }
+    }
+
+    function displayQuestion(){
+        quest.textContent = questions[0].title;
+    }
+
+     
+    function displayAnswers(){
+        for(let i = 0; i < 4 ; i++){
+            console.log(i);
+            let li = document.createElement("li");
+            li.textContent = questions[0].choices[i];
+            li.setAttribute("index", i);
+
+            //pass gradeAnwer the index of the clicked on item somehow or find a way to get it in the function
+            li.addEventListener("click", gradeAnswer);
+
+            answers.append(li);
+        }
+    }
+
+    function gradeAnswer(ind){
+        console.log("click");
+        // console.log(ind);
+
+        // // this returns thethe li
+        // console.log(this);
+
+        // console.log(this.textContent);
+
+        if(this.textContent == questions[0].answer){
+            console.log("hoo-ray");
+        }
+    }
 
 });
